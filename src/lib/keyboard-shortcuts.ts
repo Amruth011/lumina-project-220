b4a744018f6afd97b6d1d28e05c6cfaf4992c332
Amruth Kumar M <amruth.kumar.portfolio@gmail.com>
2026-06-03 17:10:12 +0000

    Changes
    Co-authored-by: Amruth011 <174258291+Amruth011@users.noreply.github.com>

--- src/lib/keyboard-shortcuts.ts
+++ src/lib/keyboard-shortcuts.ts
@@ -24,6 +24,7 @@ export const useSequenceShortcut = (sequence: string[], handler: () => void) => 
     let timeout: NodeJS.Timeout;
 
     const listener = (event: KeyboardEvent) => {
+      if (!event.key) return;
       pressedKeys.push(event.key.toLowerCase());
       clearTimeout(timeout);
 
